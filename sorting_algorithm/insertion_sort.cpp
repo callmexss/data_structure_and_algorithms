87c4ca10c85e916cb6f1b9c1aaada9307e6d12b7
xss <callmexss@126.com>
2017-12-06 22:48:12 +0800

    insertion sort recursion version

--- sorting_algorithm/insertion_sort.cpp
+++ sorting_algorithm/insertion_sort.cpp
@@ -3,7 +3,7 @@
  *      Filename: insertion_sort.cpp
  *
  *        Author: xss - callmexss@126.com
- *   Description: ---
+ *   Description: insertion sort
  *        Create: 2017-12-06 21:28:36
  *
  ***********************************************/
@@ -12,6 +12,7 @@
 #include <iostream>
 
 using namespace std;
+
 
 void InsertionSort(int num[], int len)
 {
@@ -32,10 +33,34 @@ void InsertionSort(int num[], int len)
 
 }
 
+
+void Insert(int num[], int len)
+{
+    int i, temp;
+    temp = num[len];
+
+    for (i = len - 1; i >= 0 && temp < num[i]; i--)
+    {
+        num[i+1] = num[i];
+    }
+    num[i+1] = temp;
+}
+
+
+void InsertionSortRecursion(int num[], int len)
+{
+    if (len > 0)
+    {
+        InsertionSortRecursion(num, len - 1);
+        Insert(num, len);
+    }
+}
+
 int main(int argc, const char *argv[])
 {
     int num[] = {3, 1, 4, 9, 2, 6, 5, 3};
-    InsertionSort(num, 8);
+    //InsertionSort(num, 8);
+    InsertionSortRecursion(num, 8);
     for (int i = 0; i < 8; i++)
     {
         cout << num[i] << " ";
@@ -44,3 +69,6 @@ int main(int argc, const char *argv[])
     
     return 0;
 }
+
+
+// reference: http://www.cnblogs.com/zhuorongtan/archive/2012/09/22/2698339.html
