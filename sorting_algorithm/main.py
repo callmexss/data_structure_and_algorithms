e0d8643f5263768822a3c9154d188f3d9694c3a6
callmexss <callmexss@126.com>
2019-06-26 12:35:42 +0800

    add pysnooper in Pipfile

--- sorting_algorithm/main.py
+++ sorting_algorithm/main.py
@@ -1,7 +1,8 @@
-import sortalg 
 import pyhandy.pyhandy as handy
+import sortalg
 
-li = handy.generate_nearly_ordered_array(10000, 10)
-li1 = li[:]
-handy.testSort("Selection Sort", sortalg.selection_sort, li)
-handy.testSort("Insert Sort", sortalg.insertion_sort, li)
+if __name__ == '__main__':
+    li = handy.generate_nearly_ordered_array(10000, 10)
+    li1 = li[:]
+    handy.testSort("Selection Sort", sortalg.selection_sort, li)
+    handy.testSort("Insert Sort", sortalg.insertion_sort, li)
