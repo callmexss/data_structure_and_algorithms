d35a7b791cdd825ddb3d898b678a7588cefe3466
callmexss <callmexss@126.com>
2019-07-05 13:18:56 +0800

    recursive binary search c++ version

--- search_algorithm/binary_search.cpp
+++ search_algorithm/binary_search.cpp
@@ -13,6 +13,7 @@ int binarySearch(T arr[], int n, int target)
     if (n <= 0)
         return -1;
 
+    // arr[left...right]
     int left = 0;
     int right = n - 1;
 
@@ -29,19 +30,48 @@ int binarySearch(T arr[], int n, int target)
     return -1;
 }
 
+template <typename T>
+int __binarySearch(T arr[], int l, int r, T target)
+{
+    while (l <= r)
+    {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] > target)
+            return __binarySearch(arr, l, mid - 1, target);
+        else
+            return __binarySearch(arr, mid + 1, r, target);
+    }
+    return -1;
+}
+
+template <typename T>
+int binarySearch2(T arr[], int n, T target)
+{
+    int l = 0;
+    int r = n - 1;
+
+    return __binarySearch(arr, l, r, target);
+}
+
+
 int main()
 {
     const int n = 5;
     int arr[n] = {1, 2, 3, 7, 4};
 
     assert(binarySearch(arr, n, 7) == 3);
+    assert(binarySearch2(arr, n, 7) == 3);
 
-    int n1 = 10000;
-    int *arr1 = SortTestHelper::generateRandomArray(n1, 0, n1 * 2);
+    int n1 = 50;
+    int *arr1 = SortTestHelper::generateRandomArray(n1, 0, 100);
     quickSort(arr1, n1);
-    // SortTestHelper::printArray(arr1, n1);
+    SortTestHelper::printArray(arr1, n1);
     cout << binarySearch(arr1, n1, 1) << endl;
+    cout << binarySearch2(arr1, n1, 1) << endl;
     cout << binarySearch(arr1, n1, 50) << endl;
+    cout << binarySearch2(arr1, n1, 50) << endl;
 
     
     return 0;
