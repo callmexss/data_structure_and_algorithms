f5f596706171319235e8dcefee9db7e9068224e4
callmexss <callmexss@126.com>
2019-07-20 23:41:30 +0800

    BST contain, search, traverse, remove operations

--- search_algorithm/BST.cpp
+++ search_algorithm/BST.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 #include "BST.h"
 
@@ -10,20 +11,50 @@ typedef std::pair<int, int> TreeE;
 int main(int argc, char const *argv[])
 {
     BST<int, int> tree;
-    cout << tree.size() << endl;
-    tree.insert(1, 1);
-    cout << tree.size() << endl;
+    // cout << tree.size() << endl;
+    // tree.insert(1, 1);
+    // cout << tree.size() << endl;
 
     vector<TreeE> vec{
-        TreeE(1, 2),
-        TreeE(3, 4),
-        TreeE(5, 6)
+        TreeE(1, 1),
+        TreeE(7, 7),
+        TreeE(3, 3),
+        TreeE(2, 2),
+        TreeE(4, 4),
+        TreeE(5, 5),
+        TreeE(6, 6),
     };
 
     for (auto e : vec)
         tree.insert(e.first, e.second);
 
-    cout << tree.size() << endl;
+    cout << "tree size: " << tree.size() << endl;
+
+    assert(tree.contain(3) == true);
+    assert(tree.contain(4) == true);
+
+    int *val = tree.search(3);
+    cout << "tree[3]: " << *val << endl;
+    *val = 3;
+    cout << "tree[3]: " << *val << endl;
+
+    tree.preOrder();
+    cout << endl;
+    tree.inOrder();
+    cout << endl;
+    tree.postOrder();
+    cout << endl;
+    tree.levelOrder();
+    cout << endl;
+
+    assert(tree.minimum() == 1);
+    assert(tree.maximum() == 7);
+
+    tree.removeMax();
+    tree.removeMin();
+
+    assert(tree.minimum() == 2);
+    assert(tree.maximum() == 6);
 
     return 0;
 }
--- search_algorithm/BST.h
+++ search_algorithm/BST.h
@@ -1,3 +1,5 @@
+#include <queue>
+
 template <typename Key, typename Value>
 class BST
 {
@@ -15,6 +17,14 @@ public:
             this->value = value;
             this->left = this->right = nullptr;
         }
+
+        Node(Node *node)
+        {
+            this->key = node->key;
+            this->value = node->value;
+            this->left = node->left;
+            this->right = node->right;
+        }
     };
 
     BST() : m_count(0), m_root(nullptr)
@@ -23,7 +33,7 @@ public:
 
     ~BST()
     {
-        // TODO: ~BST()
+        destroy(m_root);
     }
 
     int size() { return m_count; }
@@ -35,11 +45,104 @@ public:
         m_root = insert(m_root, key, value);
     }
 
+    bool contain(Key key)
+    {
+        contain(m_root, key);
+    }
+
+    Value *search(Key key)
+    {
+        return search(m_root, key);
+    }
+
+    void preOrder()
+    {
+        preOrder(m_root);
+    }
+
+    void inOrder()
+    {
+        inOrder(m_root);
+    }
+
+    void postOrder()
+    {
+        postOrder(m_root);
+    }
+
+    void levelOrder()
+    {
+        std::queue<Node *> q;
+        if (m_root)
+        {
+            q.push(m_root);
+            while (!q.empty())
+            {
+                Node *node = q.front();
+                q.pop();
+                std::cout << node->key << std::endl;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+        }
+    }
+
+    Key minimum()
+    {
+        assert(m_count != 0);
+        // Node *cur = m_root;
+        // while (cur)
+        // {
+        //     if (cur->left)
+        //         cur = cur->left;
+        //     else
+        //         break;
+        // }
+        // return cur->key;
+        Node *node = minimum(m_root);
+        return node->key;
+    }
+
+    Key maximum()
+    {
+        assert(m_count != 0);
+        // Node *cur = m_root;
+        // while (cur)
+        // {
+        //     if (cur->right)
+        //         cur = cur->right;
+        //     else
+        //         break;
+        // }
+        // return cur->key;
+        Node *node = maximum(m_root);
+        return node->key;
+    }
+
+    void removeMin()
+    {
+        if (m_root)
+            m_root = removeMin(m_root);
+    }
+
+    void removeMax()
+    {
+        if (m_root)
+            m_root = removeMax(m_root);
+    }
+
+    void remove(Key key)
+    {
+        m_root = remove(m_root, key);
+    }
+
 private:
-    Node* m_root;
+    Node *m_root;
     int m_count;
 
-    Node* insert(Node *node, Key key, Value value)
+    Node *insert(Node *node, Key key, Value value)
     {
         // insert Node(key, value) to BST whose root is node
         // return the new BST's root
@@ -49,11 +152,11 @@ private:
             return new Node(key, value);
         }
 
-        if (value > node->value)
+        if (key > node->key)
         {
             node->right = insert(node->right, key, value);
         }
-        else if (value < node->value)
+        else if (key < node->key)
         {
             node->left = insert(node->left, key, value);
         }
@@ -63,5 +166,157 @@ private:
         }
 
         return node;
+    }
+
+    bool contain(Node *node, Key key)
+    {
+        if (!node)
+            return false;
+
+        if (key == node->key)
+            return true;
+        else if (key > node->key)
+            return contain(node->right, key);
+        else
+            return contain(node->left, key);
+    }
+
+    Value *search(Node *node, Key key)
+    {
+        if (!node)
+            return nullptr;
+
+        if (key == node->key)
+            return &(node->value);
+        else if (key > node->key)
+            return search(node->right, key);
+        else
+            return search(node->left, key);
+    }
+
+    void preOrder(Node *node)
+    {
+        if (node)
+        {
+            std::cout << node->key << std::endl;
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+
+    void inOrder(Node *node)
+    {
+        if (node)
+        {
+            inOrder(node->left);
+            std::cout << node->key << std::endl;
+            inOrder(node->right);
+        }
+    }
+
+    void postOrder(Node *node)
+    {
+        if (node)
+        {
+            postOrder(node->left);
+            postOrder(node->right);
+            std::cout << node->key << std::endl;
+        }
+    }
+
+    void destroy(Node *node)
+    {
+        if (node)
+        {
+            destroy(node->left);
+            destroy(node->right);
+            delete node;
+            m_count--;
+        }
+    }
+
+    Node *minimum(Node *node)
+    {
+        if (node->left == nullptr)
+            return node;
+
+        return minimum(node->left);
+    }
+
+    Node *maximum(Node *node)
+    {
+        if (node->right == nullptr)
+            return node;
+
+        return maximum(node->right);
+    }
+
+    Node* removeMin(Node *node)
+    {
+        if (node->left == nullptr)
+        {
+            Node* rightNode = node->right;
+            delete node;
+            m_count--;
+            return rightNode;
+        }
+        node->left = removeMin(node->left);
+        return node;
+    }
+
+    Node* removeMax(Node* node)
+    {
+        if (node->right == nullptr)
+        {
+            Node* leftNode = node->left;
+            delete node;
+            m_count--;
+            return leftNode;
+        }
+        node->right = removeMax(node->right);
+        return node;
+    }
+
+    Node* remove(Node* node, Key key)
+    {
+        if (node == nullptr)
+            return nullptr;
+
+        if (key < node->key)
+        {
+            node->left = remove(node->left, key);
+            return node;
+        }
+        else if (key > node->key)
+        {
+            node->right = remove(node->right, key);
+            return node;
+        }
+        else // key == node->key
+        {
+            if (node->left == nullptr)
+            {
+                Node* rightNode = node->right;
+                delete node;
+                m_count--;
+                return rightNode;
+            }
+            if (node->right == nullptr)
+            {
+                Node* leftNode = node->left;
+                delete node;
+                m_count--;
+                return leftNode;
+            }
+        }
+
+        // node->left != nullptr and node->right != nullptr
+        Node *successor = new Node(minimum(node->right));
+        m_count++;
+
+        successor->right = removeMin(node->right);
+        successor->left = node->left;
+
+        return successor;
     }
 };
